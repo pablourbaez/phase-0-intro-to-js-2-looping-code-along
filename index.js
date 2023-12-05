d19b078aa05742a378acf9473f1ed33ba54b6101
Pablo <pablomurbaez@yahoo.com>
2023-12-04 22:52:44 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+console.log(writeCards(cards));
+
+countDown(5);
+function writeCards(cards) {
+    let thankYouMessages = [];
+    for (let i = 0; i < cards.length; i++) {
+      thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
